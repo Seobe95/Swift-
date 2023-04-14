31672e5e8fcffa2e3aba56af9323666fc6d27d76
MyoungSeob <woodyms@nate.com>
2023-04-14 21:44:16 +0900

    gg

--- Swift_Algorithm/프로그래머스/옹알이.swift
+++ Swift_Algorithm/프로그래머스/옹알이.swift
@@ -5,31 +5,31 @@ import Foundation
  */
 
 func solution (_ babbling: [String]) -> Int {
-  // 문제에서 주어진 가능한 옹알이 단어
-  let ableBabbling = ["aya", "ye", "woo", "ma"]
-  
-  return babbling.map { babble in
-	// 스펠링을 체크하는 str
-	var str = ""
-	// 이전 옹알이와 중복될 수 없어 이전 옹알이를 저장하는 변수
-	var prevBabbling = ""
-	// 현재 옹알이를 for문으로 분석
-	for char in babble {
-	  str = str + String(char)
-	  // 가능한 옹알이에 포함되고, 이전 옹알이와 중복되지 않는다면
-	  if ableBabbling.contains(str) && prevBabbling != str {
-		// 현재 옹알이를 이전 옹알이로
-		prevBabbling = str
-		// 현재 옹알이를 빈칸으로
-		str = ""
-	  } else if str == prevBabbling {
-		// 현재 옹알이와 이전 옹알이가 같다면 이 옹알이는 할 수 없는 옹알이
-		break
-	  }
-	}
-	// 옹알이가 비어있으면 모두 할 수 있는 옹알이, 아니라면 할 수 없는 옹알이
-	return str == "" ? 1 : 0
-	// 배열을 내 모든 요소 더하기
-  }.reduce(0) { $0 + $1 }
+    // 문제에서 주어진 가능한 옹알이 단어
+    let ableBabbling = ["aya", "ye", "woo", "ma"]
+    
+    return babbling.map { babble in
+        // 스펠링을 체크하는 str
+        var str = ""
+        // 이전 옹알이와 중복될 수 없어 이전 옹알이를 저장하는 변수
+        var prevBabbling = ""
+        // 현재 옹알이를 for문으로 분석
+        for char in babble {
+            str = str + String(char)
+            // 가능한 옹알이에 포함되고, 이전 옹알이와 중복되지 않는다면
+            if ableBabbling.contains(str) && prevBabbling != str {
+                // 현재 옹알이를 이전 옹알이로
+                prevBabbling = str
+                // 현재 옹알이를 빈칸으로
+                str = ""
+            } else if str == prevBabbling {
+                // 현재 옹알이와 이전 옹알이가 같다면 이 옹알이는 할 수 없는 옹알이
+                break
+            }
+        }
+        // 옹알이가 비어있으면 모두 할 수 있는 옹알이, 아니라면 할 수 없는 옹알이
+        return str == "" ? 1 : 0
+        // 배열을 내 모든 요소 더하기
+    }.reduce(0) { $0 + $1 }
 }
 
--- Swift_Algorithm/프로그래머스/햄버거 만들기.swift
+++ Swift_Algorithm/프로그래머스/햄버거 만들기.swift
@@ -5,17 +5,17 @@
 import Foundation
 
 func solution (_ ingredient: [Int]) -> Int {
-  var stack = Array<Int>()
-  var answer = 0
-  
-  for ing in ingredient {
-	stack.append(ing)
-	if stack.count > 3 && stack.suffix(from: stack.count - 4) == [1, 2, 3, 1] {
-	  answer += 1
-	  for _ in (stack.count - 4)...(stack.count - 1) {
-		stack.removeLast()
-	  }
-	}
-  }
-  return answer
+    var stack = Array<Int>()
+    var answer = 0
+    
+    for ing in ingredient {
+        stack.append(ing)
+        if stack.count > 3 && stack.suffix(from: stack.count - 4) == [1, 2, 3, 1] {
+            answer += 1
+            for _ in (stack.count - 4)...(stack.count - 1) {
+                stack.removeLast()
+            }
+        }
+    }
+    return answer
 }
